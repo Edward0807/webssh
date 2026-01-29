6752a4fe7209af063a9902fe9f0732c0b8d898fe
edward0807 <102449226+Edward0807@users.noreply.github.com>
2026-01-29 16:18:20 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -18,7 +18,7 @@ else
     exit 1
 fi
 
-BASE_URL="https://github.com/Edward0807/webssh/releases/download"
+BASE_URL="https://github.com/Edward0807/webssh/releases/download/v1.0.0"
 FILE_URL="${BASE_URL}/webssh_linux_${ARCH}"
 
 # 如果文件不存在，则下载
